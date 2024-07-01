d95388301dafc86245240efc73cde053e86f9d96
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-01 16:29:30 -0300

    feat: initial implementation pages Home, Cart, Details, Checkout

---
+++ pages/Details/Details.tsx
@@ -0,0 +1,7 @@
+function Details() {
+  return (
+    <h2>Details</h2>
+  );
+}
+
+export default Details;
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
+    // <Header />
+    // <Routes>
+    //   <Route
+    //     path="/"
+    //     element={ <Home
+    //       handleAddToCart={ handleAddToCart }
+    //       cartSize={ cartSize }
+    //     /> }
+    //   />
+    //   <Route
+    //     path="/carrinho"
+    //     element={ <Cart
+    //       cart={ cart }
+    //       removeItem={ removeItem }
+    //       handleIncrement={ handleIncrement }
+    //       handleDecrement={ handleDecrement }
+    //     /> }
+    //   />
+    //   <Route
+    //     path="/checkout"
+    //     element={ <Checkout
+    //       cart={ cart }
+    //       clearCart={ clearCart }
+    //     /> }
+    //   />
+    //   <Route
+    //     path="/produto/:productId"
+    //     element={ <ProductDetails
+    //       handleAddToCart={ handleAddToCart }
+    //       cartSize={ cartSize }
+    //     /> }
+    //   />
+    // </Routes>
+    // <Footer />
     <h2>online-store</h2>
   );
 }
