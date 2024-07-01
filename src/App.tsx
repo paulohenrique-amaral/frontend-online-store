8f004e1276349696eeffa5f649f3acf7b5c27fe3
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-01 16:37:08 -0300

    feat: implemented initial Routes in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,35 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import Home from '../pages/Home/Home';
+import Cart from '../pages/Cart/Cart';
+import Checkout from '../pages/Checkout/Checkout';
+import Details from '../pages/Details/Details';
 
 function App() {
   return (
-    // <Header />
-    // <Routes>
-    //   <Route
-    //     path="/"
-    //     element={ <Home
-    //       handleAddToCart={ handleAddToCart }
-    //       cartSize={ cartSize }
-    //     /> }
-    //   />
-    //   <Route
-    //     path="/carrinho"
-    //     element={ <Cart
-    //       cart={ cart }
-    //       removeItem={ removeItem }
-    //       handleIncrement={ handleIncrement }
-    //       handleDecrement={ handleDecrement }
-    //     /> }
-    //   />
-    //   <Route
-    //     path="/checkout"
-    //     element={ <Checkout
-    //       cart={ cart }
-    //       clearCart={ clearCart }
-    //     /> }
-    //   />
-    //   <Route
-    //     path="/produto/:productId"
-    //     element={ <ProductDetails
-    //       handleAddToCart={ handleAddToCart }
-    //       cartSize={ cartSize }
-    //     /> }
-    //   />
-    // </Routes>
-    // <Footer />
-    <h2>online-store</h2>
+    <>
+      {/* <Header /> */}
+      <Routes>
+        <Route
+          path="/"
+          element={ <Home /> }
+        />
+        <Route
+          path="/carrinho"
+          element={ <Cart /> }
+        />
+        <Route
+          path="/checkout"
+          element={ <Checkout /> }
+        />
+        <Route
+          path="/produto/:productId"
+          element={ <Details /> }
+        />
+      </Routes>
+      {/* <Footer /> */}
+    </>
+    // <h2>online-store</h2>
   );
 }
 
