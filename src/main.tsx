f27f6fed29a6834d139c7ee92fe3a1982b77da0c
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-02 23:27:25 -0300

    feat: initial configuration framework Material UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline } from '@mui/material';
 import Provider from './context/Provider';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Provider>
+      <CssBaseline />
       <App />
     </Provider>
   </BrowserRouter>,
