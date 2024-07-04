375fff0df2f5fd8d3edb88ee64b257c12f0a024f
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-04 16:37:55 -0300

    feat: created customize hook useFetch

---
+++ src/hook/useFetch.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { getCategories, getProductsFromCategoryAndQuery } from '../services/api';
+import { CategoryType, ProductType } from '../types/apiTypes';
+
+function useFetch() {
+  const [searchApi, setSearchApi] = useState<ProductType[]>([]);
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const fetchCategories = async () => {
+    setLoading(true);
+    const resultCategories = await getCategories();
+    setCategories(resultCategories);
+    setLoading(false);
+  };
+
+  const searchFromCategories = async (id: string) => {
+    const dataFetchCategory = await getProductsFromCategoryAndQuery(id, null);
+    setSearchApi(dataFetchCategory);
+  };
+
+  const searchFromInput = async (inputSearch: string) => {
+    const fetchInputSearch = await getProductsFromCategoryAndQuery(null, inputSearch);
+    setSearchApi(fetchInputSearch);
+  };
+
+  return {
+    searchApi,
+    categories,
+    loading,
+    fetchCategories,
+    searchFromCategories,
+    searchFromInput,
+  };
+}
+
+export default useFetch;
