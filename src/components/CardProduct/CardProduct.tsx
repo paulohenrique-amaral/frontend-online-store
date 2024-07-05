357447beca5d558c97f00b9f8f03536b07419303
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-05 10:10:11 -0300

    style: refatored style from CardProduct

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -1,10 +1,22 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { styled, Grid, Typography, Paper, Box } from '@mui/material';
+import { styled, Grid, Typography, Paper, Box, keyframes } from '@mui/material';
 import { Link } from 'react-router-dom';
+import { Rocket } from '@mui/icons-material';
 import { ProductWithQuantityType } from '../../types/apiTypes';
 import Context from '../../context/Context';
+
+const rotate = keyframes`
+  0% {
+    -webkit-transform: rotate(0);
+            transform: rotate(0);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+  }
+`;
 
 const ContainerStyled = styled(Box)(({ theme }) => ({
   // backgroundColor: 'blue',
@@ -18,7 +30,7 @@ const ContainerStyled = styled(Box)(({ theme }) => ({
 
 const Item = styled(Paper)(({ theme }) => ({
   display: 'flex',
-  justifyContent: 'center',
+  justifyContent: 'space-between',
   alignItems: 'center',
   color: theme.palette.text.primary,
   width: '100%',
@@ -27,14 +39,19 @@ const Item = styled(Paper)(({ theme }) => ({
   lineHeight: '60px',
   margin: '0.5rem',
   padding: '0.5rem',
+  transition: '0.5s',
+  '&:hover': {
+    boxShadow: '0 0 10px 0 #000',
+  },
 }));
 
 const ContentStyled = styled('div')(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   justifyContent: 'space-between',
-  alignItems: 'center',
-  // color: theme.palette.text.primary,
+  padding: '0.5rem',
+  margin: '0 0.5rem',
+  width: '100%',
 }));
 
 type CardProductProps = {
@@ -47,39 +64,79 @@ type CardProductProps = {
 };
 
 function CardProduct({ id, product, image, name, price, freight }: CardProductProps) {
+  const [animate, setAnimate] = useState(false);
   const { handleAddToCart } = useContext(Context);
   return (
     <ContainerStyled>
       <Item elevation={ 5 }>
-        <Box>
+        <Box
+          sx={ {
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            '& a': {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: '0 0.4rem',
+            },
+            '& img': {
+              width: '100px',
+            },
+          } }
+        >
           <Link to={ `/produto/${id}` }>
             <img src={ image } alt={ `Product-${name}` } />
           </Link>
         </Box>
         <ContentStyled>
-          <Box>
-            <Typography variant="h6">
-              {name}
+          <Box
+            sx={ {
+              marginBottom: '10px',
+              '& a': {
+                textDecoration: 'none',
+                color: 'black',
+              },
+            } }
+          >
+            <Typography fontFamily="monospace">
+              <Link to={ `/produto/${id}` }>
+                {name}
+              </Link>
             </Typography>
           </Box>
-          <Box>
-            <Typography variant="h6">
+          <Box
+            sx={
+            { display: 'flex', alignItems: 'baseline', gap: '0.3rem' }
+          }
+          >
+            <Typography>
               R$
-              {' '}
-              {price}
+            </Typography>
+            <Typography variant="h4">
+              {price.toLocaleString('pt-BR', { minimumFractionDigits: 2 })}
             </Typography>
           </Box>
           <Box>
             <Button
               variant="contained"
               startIcon={ <AddShoppingCartIcon /> }
-              onClick={ () => handleAddToCart(product) }
+              onClick={ () => {
+                handleAddToCart(product);
+                setAnimate(true);
+                setTimeout(() => setAnimate(false), 700);
+              } }
+              sx={ {
+                '& svg': {
+                  animation: animate ? `${rotate} .5s linear forwards` : 'none',
+                },
+              } }
             >
               Adicionar ao carrinho
             </Button>
           </Box>
           <Box>
-            <Typography variant="h6">
+            <Typography variant="h6" color="#00A650">
               {freight ? 'Frete Grátis' : ''}
             </Typography>
           </Box>
