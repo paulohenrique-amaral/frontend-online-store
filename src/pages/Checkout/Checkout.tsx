305b475ed3507e413938ab4494d6f1ccc2cabc7e
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-17 17:58:19 -0300

    refactor: refatored page checkout

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { Grid, Container, Box, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import Context from '../../context/Context';
-import { ProductWithQuantityType, FormDataType } from '../../types/apiTypes';
+import { ProductWithQuantityType } from '../../types/apiTypes';
 import CardCheckout from '../../components/CardCheckout/CardCheckout';
 import ModalFormCheckout from '../../components/ModalFormCheckout/ModalFormCheckout';
 
@@ -28,36 +28,6 @@ function Checkout() {
   const [selectedPayment, setSelectedPayment] = useState('');
 
   const { cart, clearCart } = useContext(Context);
-
-  // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-  //   event.preventDefault();
-
-  //   if (!formData.name
-  //     || formData.cpf === ''
-  //     || formData.email === ''
-  //     || formData.telefone === ''
-  //     || formData.cep === ''
-  //     || formData.endereco === '') {
-  //     setError('Campos inválidos');
-  //     return;
-  //   }
-  //   if (!selectedPayment) {
-  //     setError('Campos inválidos');
-  //     return;
-  //   }
-  //   setError('');
-  //   window.location.href = '/';
-  //   clearCart();
-  // };
-
-  // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const { name, value } = event.target;
-  //   setFormdata({
-  //     ...formData,
-  //     [name]: value,
-  //   });
-  //   setError('');
-  // };
 
   return (
     <Container maxWidth="lg">
@@ -84,130 +54,6 @@ function Checkout() {
           </Box>
         </Grid>
         <ModalFormCheckout open={ open } setOpen={ setOpen } />
-        {/* <Grid item xs={ 12 } md={ 12 }>
-          <Typography variant="h5">
-            Informações do Comprador
-          </Typography>
-        </Grid>
-        <Grid item>
-          <form
-            onSubmit={ handleSubmit }
-          >
-            <label>
-              <input
-                data-testid="checkout-fullname"
-                name="name"
-                type="text"
-                placeholder="Nome Completo"
-                value={ formData.name }
-                onChange={ handleChange }
-              />
-            </label>
-            <label>
-              <input
-                data-testid="checkout-cpf"
-                name="cpf"
-                type="text"
-                placeholder="CPF"
-                value={ formData.cpf }
-                onChange={ handleChange }
-              />
-            </label>
-            <label>
-              <input
-                data-testid="checkout-email"
-                name="email"
-                type="text"
-                placeholder="Email"
-                value={ formData.email }
-                onChange={ handleChange }
-              />
-            </label>
-            <label>
-              <input
-                data-testid="checkout-phone"
-                name="telefone"
-                type="text"
-                placeholder="Telefone"
-                value={ formData.telefone }
-                onChange={ handleChange }
-              />
-            </label>
-            <label>
-              <input
-                data-testid="checkout-cep"
-                name="cep"
-                type="text"
-                placeholder="CEP"
-                value={ formData.cep }
-                onChange={ handleChange }
-              />
-            </label>
-            <label>
-              <input
-                data-testid="checkout-address"
-                name="endereco"
-                type="text"
-                placeholder="Endereço"
-                value={ formData.endereco }
-                onChange={ handleChange }
-              />
-            </label>
-            <div>
-              <p>Método de Pagamento</p>
-              <label>
-                Boleto
-                <input
-                  data-testid="ticket-payment"
-                  name="payment"
-                  type="radio"
-                  value="boleto"
-                  checked={ selectedPayment === 'boleto' }
-                  onChange={ () => setSelectedPayment('boleto') }
-                />
-              </label>
-              <label>
-                Visa
-                <input
-                  data-testid="visa-payment"
-                  name="payment"
-                  type="radio"
-                  value="visa"
-                  checked={ selectedPayment === 'visa' }
-                  onChange={ () => setSelectedPayment('visa') }
-                />
-              </label>
-              <label>
-                MasterCard
-                <input
-                  data-testid="master-payment"
-                  name="payment"
-                  type="radio"
-                  value="mastercard"
-                  checked={ selectedPayment === 'mastercard' }
-                  onChange={ () => setSelectedPayment('mastercard') }
-                />
-              </label>
-              <label>
-                Elo
-                <input
-                  data-testid="elo-payment"
-                  name="payment"
-                  type="radio"
-                  value="elo"
-                  checked={ selectedPayment === 'elo' }
-                  onChange={ () => setSelectedPayment('elo') }
-                />
-              </label>
-            </div>
-            <button
-              type="submit"
-            >
-              Comprar
-            </button>
-          </form>
-          {error && <p>{ error }</p>}
-        </Grid> */}
       </Grid>
       <Box
         sx={ {
