5235305eca4cd305df8859ef1747db53823e8078
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-22 16:37:09 -0300

    refactor: refactor and update StarRating

--- src/components/StarRating/StarRating.tsx
+++ src/components/StarRating/StarRating.tsx
@@ -1,6 +1,46 @@
+import { useContext, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Rating from '@mui/material/Rating';
+import Typography from '@mui/material/Typography';
+import Context from '../../context/Context';
+
 function StarRating() {
+  const [rating, setRating] = useState(0);
+  const { productId } = useParams<string>();
+  const productIdAsserted = productId!;
+  const { productReviews } = useContext(Context);
+
+  const reviews = productReviews.filter((review) => review.id === productIdAsserted);
+
+  const evaluationAverage = reviews
+    .reduce((acc, review) => acc + review.rating, 0) / reviews.length;
+
+  useEffect(() => {
+    setRating(evaluationAverage);
+  }, [evaluationAverage]);
+
   return (
-    <h2>@</h2>
+    <Box
+      sx={ {
+        gap: 2,
+        display: 'flex',
+        alignItems: 'center',
+        '& > legend': { mt: 2 },
+      } }
+    >
+      <Rating
+        name="half-rating-read"
+        value={ rating }
+        precision={ 0.5 }
+        readOnly
+      />
+      <Typography
+        variant="body1"
+      >
+        {rating ? rating.toFixed(1) : 'Sem Avaliações'}
+      </Typography>
+    </Box>
   );
 }
 
