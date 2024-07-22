5a8384d58be0e46ad0ff2f3aa68b29a6ffe4742e
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-22 16:38:40 -0300

    refactor: refactor and update page Details

--- src/pages/Details/Details.tsx
+++ src/pages/Details/Details.tsx
@@ -1,141 +1,232 @@
 import { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
+import { Container, Grid, Box, Typography, Button } from '@mui/material';
+import AddReactionIcon from '@mui/icons-material/AddReaction';
 import Context from '../../context/Context';
 import { getProductById } from '../../services/api';
-import ShoppingCart from '../../components/ShoppingCart/ShoppingCart';
 import Loading from '../../components/Loading/Loading';
+import StarRating from '../../components/StarRating/StarRating';
+import { ProductDetailType } from '../../types/apiTypes';
+import FormEvaluation from '../../components/FormEvaluation/FormEvaluation';
+import ReviewsSection from '../../components/ReviewsSection/ReviewsSection';
 
-const initialformRating = {
-  email: '',
-  textarea: '',
+const initialProductDetails = {
+  id: '',
+  title: '',
+  thumbnail: '',
+  price: 0,
+  shipping: { free_shipping: false },
+  warranty: '',
+  pictures: [
+    { url: '' },
+  ],
+  quantity: 0,
+  available_quantity: 0,
 };
 
 function Details() {
   const { productId } = useParams<string>();
-  const [product, setProduct] = useState<ProductType | null>(null);
+  const productIdAsserted = productId!;
+  const [product, setProduct] = useState<ProductDetailType>(initialProductDetails);
   const [isLoading, setIsLoading] = useState(true);
 
-  const [selectedStar, setSelectedStar] = useState<number>();
-  const [formRating, setFormRating] = useState<ProductRatingType>(initialformRating);
-  const [ratingError, setRatingError] = useState('');
-
-  const { handleAddToCart, cartSize } = useContext(Context);
+  const { handleAddToCart, productReviews } = useContext(Context);
 
   useEffect(() => {
-    if (productId) {
-      getProductById(productId)
-        .then((data) => {
-          setProduct(data);
-          setIsLoading(false);
-        })
-        .catch((error) => {
-          console.error('Failed to fetch product by ID:', error);
-        });
-    }
-  }, [productId]);
-
-  const validateEmail = () => {
-    const emailOk = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    return emailOk.test(formRating.email);
-  };
-
-  const handleChange = (event:
-  React.ChangeEvent<HTMLInputElement>
-  | React.ChangeEvent<HTMLTextAreaElement>) => {
-    const { name, value } = event.target;
-    setFormRating({
-      ...formRating,
-      [name]: value,
-    });
-    setRatingError('');
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (!validateEmail()
-      || selectedStar === undefined) {
-      setRatingError('Campos inválidos');
-      return;
-    }
-    const newReview = {
-      email: formRating.email,
-      text: formRating.textarea,
-      rating: selectedStar,
-    };
-    if (productId) {
-      const existingReviews = JSON.parse(localStorage.getItem(productId) || '[]');
-      existingReviews.push(newReview);
-      localStorage.setItem(productId, JSON.stringify(existingReviews));
-    }
-    setFormRating(initialformRating);
-    setSelectedStar(undefined);
-    setRatingError('');
-  };
+    getProductById(productIdAsserted)
+      .then((data) => {
+        setProduct(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error('Failed to fetch product by ID:', error);
+      });
+  }, [productIdAsserted]);
 
   if (isLoading) return <Loading />;
 
   return (
-    <div>
-      <h2>{product?.title}</h2>
-      <img
-        src={ product?.thumbnail }
-        alt="Produto"
-      />
-      <p>{`R$ ${product?.price}`}</p>
-      <button
-        type="button"
-        onClick={ () => handleAddToCart(product) }
-      >
-        Adicionar ao carrinho
-      </button>
-      <ShoppingCart />
-      <div>
-        <h2>Avaliações</h2>
-        <form
-          onSubmit={ handleSubmit }
+    <Container maxWidth="lg">
+      <Grid container sx={ { marginBottom: '100px' } }>
+        <Grid
+          item
+          xs={ 12 }
+          md={ 8 }
+          sx={ {
+            backgroundColor: '#fff',
+          } }
         >
-          <label htmlFor="product-detail-email">
-            email
-            <input
-              id="product-detail-email"
-              name="email"
-              type="text"
-              value={ formRating.email }
-              onChange={ handleChange }
-              placeholder="Email"
-            />
-          </label>
-          {/* <div>
-            {starItem.slice(1).map((index) => (
-              <StarRating
-                index={ index }
-                onClick={ () => onClickStar(index) }
-                key={ `star_${index}` }
-                isActive={ index <= (selectedStar || 0) }
-              />
-            ))}
-          </div> */}
-          <label htmlFor="product-detail-evaluation">
-            avaliação
-            <textarea
-              id="product-detail-evaluation"
-              name="textarea"
-              value={ formRating.textarea }
-              onChange={ handleChange }
-              cols={ 30 }
-              rows={ 10 }
-            />
-          </label>
-          <button
-            type="submit"
+          <Box
+            sx={ {
+              width: '90%',
+              display: 'flex',
+              justifyContent: 'center',
+              margin: '20px 0px',
+            } }
           >
-            Avaliar
-          </button>
-        </form>
-        {/* {ratingError && <p>{ ratingError }</p>} */}
-        {/* {renderReviews()} */}
-      </div>
-    </div>
+            <img
+              style={ { maxWidth: '100%', height: 'auto' } }
+              src={ product?.pictures[0].url }
+              alt="Produto"
+            />
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={ 12 }
+          md={ 4 }
+          sx={ {
+            backgroundColor: '#fff',
+          } }
+        >
+          <Box
+            sx={ {
+              display: 'flex',
+              flexDirection: 'column',
+              gap: '10px',
+              margin: '20px 0px',
+              padding: '10px',
+            } }
+          >
+            <Typography variant="h5">
+              {product?.title}
+            </Typography>
+            <Box>
+              <StarRating />
+            </Box>
+            <Box
+              sx={ {
+                gap: '10px',
+                display: 'flex',
+                alignItems: 'baseline',
+              } }
+            >
+              <Typography
+                variant="overline"
+                sx={ {
+                  textDecoration: 'line-through',
+                } }
+              >
+                R$
+                {
+                  (product.price + product.price * 0.30).toLocaleString('pt-BR', {
+                    minimumFractionDigits: 2,
+                  })
+                }
+              </Typography>
+            </Box>
+            <Box
+              sx={ {
+                gap: '10px',
+                display: 'flex',
+                alignItems: 'baseline',
+              } }
+            >
+              <Typography variant="body1">
+                R$
+              </Typography>
+              <Typography variant="h6">
+                {
+                  product?.price.toLocaleString('pt-BR', {
+                    minimumFractionDigits: 2,
+                  })
+                }
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="body2">
+                {product?.warranty}
+              </Typography>
+            </Box>
+            <Box>
+              <Typography variant="h6" color="#00A650">
+                {product?.shipping.free_shipping ? 'Frete Grátis' : ''}
+              </Typography>
+            </Box>
+            <Button
+              variant="contained"
+              endIcon={ <AddReactionIcon /> }
+              type="button"
+              onClick={ () => handleAddToCart(product) }
+            >
+              Eu quero
+            </Button>
+          </Box>
+        </Grid>
+        <Grid
+          item
+          xs={ 12 }
+          md={ 12 }
+          sx={ {
+            padding: '20px',
+            margin: '20px 0px',
+            backgroundColor: '#fff',
+            borderRadius: '10px',
+          } }
+        >
+          <Typography variant="h5">
+            Detalhes
+          </Typography>
+          <Typography variant="body1">
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Illo eveniet suscipit,
+            porro veniam sapiente quas labore ut et.
+            Itaque iste delectus esse sunt commodi.
+            Sapiente veniam iure aspernatur repudiandae
+            odit.
+            Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit. Illo eveniet suscipit,
+            porro veniam sapiente quas labore ut et.
+            Itaque iste delectus esse sunt commodi.
+            Sapiente veniam iure aspernatur repudiandae
+          </Typography>
+        </Grid>
+        <Grid
+          item
+          xs={ 12 }
+          md={ 12 }
+          sx={ {
+            padding: '20px',
+            margin: '0px 0px 20px 0px',
+            backgroundColor: '#fff',
+            borderRadius: '10px',
+          } }
+        >
+          <Grid
+            item
+            xs={ 12 }
+            md={ 7 }
+            sx={ {
+              paddingRight: '30px',
+            } }
+          >
+            <FormEvaluation />
+          </Grid>
+        </Grid>
+        <Grid
+          item
+          xs={ 12 }
+          md={ 12 }
+          sx={ {
+            padding: '20px',
+            margin: '0px 0px 20px 0px',
+            backgroundColor: '#fff',
+            borderRadius: '10px',
+          } }
+        >
+
+          {productReviews.length > 0 ? (
+
+            <ReviewsSection />
+          ) : (
+
+            <Typography variant="h6">
+              Nenhuma avaliação disponível
+            </Typography>
+          )}
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
