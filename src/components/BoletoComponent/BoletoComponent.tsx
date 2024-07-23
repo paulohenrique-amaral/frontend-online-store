9f8fc723b82e65397de3c44b4d721ca8bcda2ce0
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-22 21:45:26 -0300

    refactor: refatored and created style file

--- src/components/BoletoComponent/BoletoComponent.tsx
+++ src/components/BoletoComponent/BoletoComponent.tsx
@@ -1,54 +1,11 @@
 import { useContext } from 'react';
 import ViewWeekIcon from '@mui/icons-material/ViewWeek';
-import { styled, Box, keyframes, Typography, Button } from '@mui/material';
+import { Typography, Button } from '@mui/material';
 import Context from '../../context/Context';
-
-const animation = keyframes`
-  0% {
-    -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1);
-  }
-  30% {
-    -webkit-transform: scale3d(1.25, 0.75, 1);
-            transform: scale3d(1.25, 0.75, 1);
-  }
-  40% {
-    -webkit-transform: scale3d(0.75, 1.25, 1);
-            transform: scale3d(0.75, 1.25, 1);
-  }
-  50% {
-    -webkit-transform: scale3d(1.15, 0.85, 1);
-            transform: scale3d(1.15, 0.85, 1);
-  }
-  65% {
-    -webkit-transform: scale3d(0.95, 1.05, 1);
-            transform: scale3d(0.95, 1.05, 1);
-  }
-  75% {
-    -webkit-transform: scale3d(1.05, 0.95, 1);
-            transform: scale3d(1.05, 0.95, 1);
-  }
-  100% {
-    -webkit-transform: scale3d(1, 1, 1);
-            transform: scale3d(1, 1, 1);
-  }
-`;
-
-const BoxStyled = styled('header')(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  width: '100%',
-  justifyContent: 'center',
-  alignItems: 'center',
-  padding: '100px',
-  '& svg': {
-    color: theme.palette.primary.main,
-    animation: `${animation} 1s`,
-  },
-}));
+import { BoxStyled } from './BoletoComponentStyled';
 
 function BoletoComponent() {
-  const { personData, setPersonData, setEtapaAtual } = useContext(Context);
+  const { setEtapaAtual } = useContext(Context);
   return (
     <BoxStyled>
       <Typography variant="body1">
---
+++ src/components/BoletoComponent/BoletoComponentStyled.ts
@@ -0,0 +1,45 @@
+import { styled, keyframes } from '@mui/material';
+
+const animation = keyframes`
+  0% {
+    -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+  }
+  30% {
+    -webkit-transform: scale3d(1.25, 0.75, 1);
+            transform: scale3d(1.25, 0.75, 1);
+  }
+  40% {
+    -webkit-transform: scale3d(0.75, 1.25, 1);
+            transform: scale3d(0.75, 1.25, 1);
+  }
+  50% {
+    -webkit-transform: scale3d(1.15, 0.85, 1);
+            transform: scale3d(1.15, 0.85, 1);
+  }
+  65% {
+    -webkit-transform: scale3d(0.95, 1.05, 1);
+            transform: scale3d(0.95, 1.05, 1);
+  }
+  75% {
+    -webkit-transform: scale3d(1.05, 0.95, 1);
+            transform: scale3d(1.05, 0.95, 1);
+  }
+  100% {
+    -webkit-transform: scale3d(1, 1, 1);
+            transform: scale3d(1, 1, 1);
+  }
+`;
+
+export const BoxStyled = styled('header')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  width: '100%',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '100px',
+  '& svg': {
+    color: theme.palette.primary.main,
+    animation: `${animation} 1s`,
+  },
+}));
