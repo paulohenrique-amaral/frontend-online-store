bfb539135782df2201914407f5d5aef172e95e44
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-22 21:54:41 -0300

    refactor: refatored component and created style file

--- src/components/CardProduct/CardProduct.tsx
+++ src/components/CardProduct/CardProduct.tsx
@@ -1,78 +1,11 @@
 import { useContext, useState } from 'react';
-import { useNavigate, useLocation, Link } from 'react-router-dom';
+import { useLocation, Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
-import { styled, Typography, Paper, Box, keyframes } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 import { ProductWithQuantityType } from '../../types/apiTypes';
 import Context from '../../context/Context';
-
-const rotate = keyframes`
-  0% {
-    -webkit-transform: rotate(0);
-            transform: rotate(0);
-  }
-  100% {
-    -webkit-transform: rotate(360deg);
-            transform: rotate(360deg);
-  }
-`;
-
-const scaleOutVertical = keyframes`
-  0% {
-    -webkit-transform: scale(1);
-            transform: scale(1);
-    -webkit-transform-origin: 50% 0%;
-            transform-origin: 50% 0%;
-    opacity: 1;
-  }
-  100% {
-    -webkit-transform: scale(0);
-            transform: scale(0);
-    -webkit-transform-origin: 50% 0%;
-            transform-origin: 50% 0%;
-    opacity: 1;
-  }
-`;
-
-const ContainerStyled = styled(Box)(({ theme }) => ({
-  color: theme.palette.primary.contrastText,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-}));
-
-type ItemProps = {
-  remove: boolean;
-};
-
-const Item = styled(Paper, {
-  shouldForwardProp: (prop) => prop !== 'remove',
-})<ItemProps>(({ theme, remove }) => ({
-  position: 'relative',
-  display: 'flex',
-  justifyContent: 'flex-start',
-  alignItems: 'center',
-  color: theme.palette.text.primary,
-  width: '90%',
-  lineHeight: '60px',
-  margin: '0.5rem',
-  padding: '0.5rem',
-  transition: '0.5s',
-  animation: remove ? `${scaleOutVertical} 0.2s linear forwards` : 'none',
-  '&:hover': {
-    boxShadow: '0 0 10px 0 #000',
-  },
-}));
-
-const ContentStyled = styled('div')(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'start',
-  justifyContent: 'space-around',
-  padding: '0.5rem',
-  margin: '0 0.5rem',
-  width: 'calc(100% - 220px)',
-}));
+import { ContainerStyled, Item, ContentStyled, rotate } from './CardProductStyled';
 
 type CardProductProps = {
   id: string;
@@ -93,7 +26,6 @@ function CardProduct({ id, product, image, name, price, freight }: CardProductPr
     incrementItemQuantity,
   } = useContext(Context);
 
-  const navigate = useNavigate();
   const location = useLocation();
 
   const showBtn = location.pathname === '/carrinho';
---
+++ src/components/CardProduct/CardProductStyled.ts
@@ -0,0 +1,69 @@
+import { styled, Typography, Paper, Box, keyframes } from '@mui/material';
+
+export const rotate = keyframes`
+  0% {
+    -webkit-transform: rotate(0);
+            transform: rotate(0);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+  }
+`;
+
+const scaleOutVertical = keyframes`
+  0% {
+    -webkit-transform: scale(1);
+            transform: scale(1);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+    opacity: 1;
+  }
+  100% {
+    -webkit-transform: scale(0);
+            transform: scale(0);
+    -webkit-transform-origin: 50% 0%;
+            transform-origin: 50% 0%;
+    opacity: 1;
+  }
+`;
+
+export const ContainerStyled = styled(Box)(({ theme }) => ({
+  color: theme.palette.primary.contrastText,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}));
+
+type ItemProps = {
+  remove: boolean;
+};
+
+export const Item = styled(Paper, {
+  shouldForwardProp: (prop) => prop !== 'remove',
+})<ItemProps>(({ theme, remove }) => ({
+  position: 'relative',
+  display: 'flex',
+  justifyContent: 'flex-start',
+  alignItems: 'center',
+  color: theme.palette.text.primary,
+  width: '90%',
+  lineHeight: '60px',
+  margin: '0.5rem',
+  padding: '0.5rem',
+  transition: '0.5s',
+  animation: remove ? `${scaleOutVertical} 0.2s linear forwards` : 'none',
+  '&:hover': {
+    boxShadow: '0 0 10px 0 #000',
+  },
+}));
+
+export const ContentStyled = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'start',
+  justifyContent: 'space-around',
+  padding: '0.5rem',
+  margin: '0 0.5rem',
+  width: 'calc(100% - 220px)',
+}));
