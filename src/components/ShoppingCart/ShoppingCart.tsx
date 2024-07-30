c45a8ce9aaeb477f8764277991726b2b3bc8e115
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-29 21:24:48 -0300

    refactor: update style for ShoppingCart

--- src/components/ShoppingCart/ShoppingCart.tsx
+++ src/components/ShoppingCart/ShoppingCart.tsx
@@ -4,18 +4,34 @@ import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import Context from '../../context/Context';
 import { StyledBadge } from './ShoppingCartStyled';
+import CartDrawer from '../CartDrawer/CartDrawer';
 
 function ShoppingCart() {
-  const { cartSize } = useContext(Context);
+  const { cartSize, toggleCartDrawer } = useContext(Context);
 
   return (
-    <Link to="/carrinho">
-      <IconButton aria-label="cart">
-        <StyledBadge badgeContent={ cartSize } color="success">
-          <ShoppingCartIcon />
+    <>
+      <IconButton
+        onClick={ () => toggleCartDrawer(true) }
+        aria-label="cart"
+      >
+        <StyledBadge
+          badgeContent={ cartSize }
+          color="info"
+          sx={ {
+            '& .MuiBadge-badge': {
+              right: -5,
+              top: 1,
+              border: 'px solid white',
+              padding: '0 4px',
+            },
+          } }
+        >
+          <ShoppingCartIcon style={ { fontSize: '2rem' } } />
         </StyledBadge>
       </IconButton>
-    </Link>
+      <CartDrawer />
+    </>
   );
 }
 
