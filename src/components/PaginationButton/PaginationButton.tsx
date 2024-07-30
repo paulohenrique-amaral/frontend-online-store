cf2a845ee59fffebdbc5dbe82b6d04e8f2c33d92
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-30 16:02:39 -0300

    refactor: adding prop in the component

--- src/components/PaginationButton/PaginationButton.tsx
+++ src/components/PaginationButton/PaginationButton.tsx
@@ -5,14 +5,16 @@ import Stack from '@mui/material/Stack';
 type PaginationButtonProps = {
   count: number;
   onChange: (event: React.ChangeEvent<unknown>, value: number) => void;
+  page: number;
 };
 
-function PaginationButton({ count, onChange }: PaginationButtonProps) {
+function PaginationButton({ count, onChange, page }: PaginationButtonProps) {
   return (
     <Stack spacing={ 2 }>
       <Pagination
         count={ count }
         shape="rounded"
+        page={ page }
         onChange={ onChange }
       />
     </Stack>
