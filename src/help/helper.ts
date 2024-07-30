0f4f602182115b1fd2dd13c604f1064f579850c4
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-29 21:22:11 -0300

    feat: created function for calculate total cart in the helper file

--- src/help/helper.ts
+++ src/help/helper.ts
@@ -1,3 +1,5 @@
+import { ProductWithQuantityType } from '../types/apiTypes';
+
 export const scrollToSection = (page: string) => {
   if (page) {
     const section = document.getElementById(page);
@@ -19,4 +21,12 @@ export const initialProductDetails = {
   ],
   quantity: 0,
   available_quantity: 0,
+};
+
+export const handleCalculateTotalCart = (cart: ProductWithQuantityType[]) => {
+  return cart.reduce((acc, item) => acc + (item.price * item.quantity), 0)
+    .toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    });
 };
