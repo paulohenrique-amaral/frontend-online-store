06abaae6277b20ce0ff6bb8b1ceb351f425557f3
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-29 21:31:03 -0300

    refactor: update component adding Grid resume

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -5,12 +5,13 @@ import Button from '@mui/material/Button';
 import Context from '../../context/Context';
 import CardProduct from '../../components/CardProduct/CardProduct';
 import EmptyCart from '../../components/EmptyCart/EmptyCart';
+import { handleCalculateTotalCart } from '../../help/helper';
 
 function Cart() {
   const { cart } = useContext(Context);
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="lg">
       <Typography
         variant="h4"
         sx={ {
@@ -20,37 +21,131 @@ function Cart() {
       >
         Meu Carrinho de Compras
       </Typography>
-      {cart.length > 0 ? (
+      <Grid
+        container
+        sx={ {
+          display: 'flex',
+          justifyContent: 'space-around',
+        } }
+      >
         <Grid
           item
           xs={ 12 }
-          // sm={ 8 }
-          md={ 12 }
+          md={ 6 }
           sx={ {
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            // alignItems: 'center',
+            height: '600px',
+            overflow: 'auto',
+            backgroundColor: 'white',
             marginTop: '1em',
-            padding: '1em',
+            borderRadius: '5px',
+            boxShadow: `rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, 
+            rgba(9, 30, 66, 0.08) 0px 0px 0px 1px`,
           } }
         >
-          {cart.map((product: any) => (
-            <div key={ product.id }>
-              <CardProduct
-                id={ product.id }
-                product={ product }
-                image={ product.thumbnail }
-                name={ product.title }
-                price={ product.price }
-                freight={ product.shipping.free_shipping }
-              />
-            </div>
-          ))}
+          {cart.length > 0 ? (
+            <Grid
+              item
+              xs={ 12 }
+              md={ 12 }
+              sx={ {
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                marginTop: '1em',
+                padding: '1em',
+              } }
+            >
+              {cart.map((product: any) => (
+                <div key={ product.id }>
+                  <CardProduct
+                    id={ product.id }
+                    product={ product }
+                    image={ product.thumbnail }
+                    name={ product.title }
+                    price={ product.price }
+                    freight={ product.shipping.free_shipping }
+                  />
+                </div>
+              ))}
+            </Grid>
+          ) : (
+            <EmptyCart />
+          )}
         </Grid>
-      ) : (
-        <EmptyCart />
-      )}
+        <Grid
+          item
+          xs={ 12 }
+          md={ 5 }
+          sx={ {
+            height: '600px',
+            backgroundColor: 'white',
+            marginTop: '1em',
+            padding: '1em',
+            borderRadius: '5px',
+            boxShadow: `rgba(9, 30, 66, 0.25) 0px 4px 8px -2px, 
+             rgba(9, 30, 66, 0.08) 0px 0px 0px 1px`,
+          } }
+        >
+          <Typography
+            variant="h5"
+          >
+            Resumo do Pedido
+          </Typography>
+          {cart.map((product: any, index: number) => (
+            <Grid
+              container
+              key={ product.id }
+              sx={ {
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+                padding: '0.5em',
+              } }
+            >
+              <Typography
+                variant="body1"
+              >
+                {index + 1}
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={ {
+                  width: '50%',
+                  whiteSpace: 'nowrap',
+                  overflow: 'hidden',
+                  textOverflow: 'ellipsis',
+                } }
+              >
+                {product.title}
+              </Typography>
+              <Typography
+                variant="body1"
+              >
+                {product.price.toLocaleString('pt-br', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })}
+              </Typography>
+              <Typography
+                variant="body1"
+              >
+                {product.quantity}
+                x
+              </Typography>
+            </Grid>
+          ))}
+          <Typography
+            variant="h5"
+            sx={ {
+              marginTop: '1em',
+            } }
+          >
+            Total
+            {' '}
+            {handleCalculateTotalCart(cart)}
+          </Typography>
+        </Grid>
+      </Grid>
       <Box
         sx={ {
           display: 'flex',
