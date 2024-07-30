3bcc2186782ecc430b14331469563420cb3328d3
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-29 21:26:56 -0300

    feat: created component Offers

---
+++ src/components/Offers/Offers.tsx
@@ -0,0 +1,92 @@
+import { useEffect, useState } from 'react';
+import { Grid, Box, Container, Typography } from '@mui/material';
+import OffersGrid from '../OffersGrid/OffersGrid';
+import { CategoryType } from '../../types/apiTypes';
+import { getCategories } from '../../services/api';
+import Loading from '../Loading/Loading';
+
+function Offers() {
+  const [randomCategories, setRandomCategories] = useState<CategoryType[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const fetchAndSetCategories = async () => {
+      const categories = await getCategories();
+      const shuffledCategories = categories.sort(() => Math.random() - 0.5);
+      const selectedCategories = shuffledCategories.slice(0, 5);
+      setRandomCategories(selectedCategories);
+    };
+    fetchAndSetCategories();
+    setLoading(false);
+  }, []);
+
+  return (
+    <Container maxWidth="lg">
+      <Grid container>
+        <Grid
+          item
+          xs={ 12 }
+          md={ 12 }
+          sx={ {
+            padding: '1rem',
+            backgroundColor: 'background.paper',
+          } }
+        >
+          <Typography
+            variant="h4"
+            align="center"
+            textTransform="uppercase"
+          >
+            Ofertas do dia
+          </Typography>
+        </Grid>
+        {!loading ? (
+          randomCategories.map((category, index) => (
+            <Grid
+              key={ index }
+              item
+              xs={ 12 }
+              md={ 12 }
+              sx={ {
+                padding: '1rem',
+                backgroundColor: 'background.paper',
+              } }
+            >
+              <Box
+                sx={ {
+                  marginBottom: '1rem',
+                  height: '370px',
+                } }
+              >
+                <Typography
+                  variant="body1"
+                  align="left"
+                  textTransform="uppercase"
+                  style={ { fontWeight: 'bold' } }
+                >
+                  {`Ofertas e Novidades - ${category.name}`}
+                </Typography>
+                <OffersGrid category={ category } />
+              </Box>
+            </Grid>
+          ))
+        ) : (
+          <Box
+            sx={ {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+              width: '100vh',
+            } }
+          >
+            <Loading />
+          </Box>
+        )}
+      </Grid>
+    </Container>
+  );
+}
+
+export default Offers;
