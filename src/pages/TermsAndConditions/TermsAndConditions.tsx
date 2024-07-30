d98fe13f9243499d6bd0b9b33694c1fc29d46066
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-29 21:25:24 -0300

    feat: created page TermsAndConditions

---
+++ src/pages/TermsAndConditions/TermsAndConditions.tsx
@@ -0,0 +1,33 @@
+function TermsAndConditions() {
+  return (
+    <div>
+      <h1>Terms and Conditions</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, est nec tincidunt congue, nunc nisl gravida nisl, sed
+        consequat sem eros nec velit. Sed nec metus id lacus lacinia fermentum
+        vel et nunc. Proin nec justo in elit auctor tincidunt. Nullam nec
+        ultricies justo. Nullam nec metus vel nisi tincidunt tincidunt. Integer
+        nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+        scelerisque ultricies. Nulla facilisi. Nullam nec libero nec nunc
+        ultricies eleifend. Nullam nec libero nec nunc ultricies eleifend.
+        Integer nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+        scelerisque ultricies. Nulla facilisi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        consectetur, est nec tincidunt congue, nunc nisl gravida nisl, sed
+        consequat sem eros nec velit. Sed nec metus id lacus lacinia fermentum
+        vel et nunc. Proin nec justo in elit auctor tincidunt. Nullam nec
+        ultricies justo. Nullam nec metus vel nisi tincidunt tincidunt. Integer
+        nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+        scelerisque ultricies. Nulla facilisi. Nullam nec libero nec nunc
+        ultricies eleifend. Nullam nec libero nec nunc ultricies eleifend.
+        Integer nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+        scelerisque ultricies. Nulla facilisi.
+      </p>
+    </div>
+  );
+}
+
+export default TermsAndConditions;
