3e5f2d58b6ebe326476e05da123f7dccd0bbb41a
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-31 19:44:04 -0300

    refactor: update style and refactor page TermsAndConditions

--- src/pages/TermsAndConditions/TermsAndConditions.tsx
+++ src/pages/TermsAndConditions/TermsAndConditions.tsx
@@ -1,32 +1,44 @@
+import { Container, Grid } from '@mui/material';
+
 function TermsAndConditions() {
   return (
-    <div>
-      <h1>Terms and Conditions</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-        consectetur, est nec tincidunt congue, nunc nisl gravida nisl, sed
-        consequat sem eros nec velit. Sed nec metus id lacus lacinia fermentum
-        vel et nunc. Proin nec justo in elit auctor tincidunt. Nullam nec
-        ultricies justo. Nullam nec metus vel nisi tincidunt tincidunt. Integer
-        nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
-        scelerisque ultricies. Nulla facilisi. Nullam nec libero nec nunc
-        ultricies eleifend. Nullam nec libero nec nunc ultricies eleifend.
-        Integer nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
-        scelerisque ultricies. Nulla facilisi.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-        consectetur, est nec tincidunt congue, nunc nisl gravida nisl, sed
-        consequat sem eros nec velit. Sed nec metus id lacus lacinia fermentum
-        vel et nunc. Proin nec justo in elit auctor tincidunt. Nullam nec
-        ultricies justo. Nullam nec metus vel nisi tincidunt tincidunt. Integer
-        nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
-        scelerisque ultricies. Nulla facilisi. Nullam nec libero nec nunc
-        ultricies eleifend. Nullam nec libero nec nunc ultricies eleifend.
-        Integer nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
-        scelerisque ultricies. Nulla facilisi.
-      </p>
-    </div>
+    <Container maxWidth="sm">
+      <Grid
+        item
+        xs={ 12 }
+        md={ 12 }
+        sx={ {
+          heigth: '100vh',
+          marginBottom: '5rem',
+        } }
+      >
+        <h1>Terms and Conditions</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          consectetur, est nec tincidunt congue, nunc nisl gravida nisl, sed
+          consequat sem eros nec velit. Sed nec metus id lacus lacinia fermentum
+          vel et nunc. Proin nec justo in elit auctor tincidunt. Nullam nec
+          ultricies justo. Nullam nec metus vel nisi tincidunt tincidunt. Integer
+          nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+          scelerisque ultricies. Nulla facilisi. Nullam nec libero nec nunc
+          ultricies eleifend. Nullam nec libero nec nunc ultricies eleifend.
+          Integer nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+          scelerisque ultricies. Nulla facilisi.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+          consectetur, est nec tincidunt congue, nunc nisl gravida nisl, sed
+          consequat sem eros nec velit. Sed nec metus id lacus lacinia fermentum
+          vel et nunc. Proin nec justo in elit auctor tincidunt. Nullam nec
+          ultricies justo. Nullam nec metus vel nisi tincidunt tincidunt. Integer
+          nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+          scelerisque ultricies. Nulla facilisi. Nullam nec libero nec nunc
+          ultricies eleifend. Nullam nec libero nec nunc ultricies eleifend.
+          Integer nec libero et nunc ultricies eleifend. Nullam nec metus nec elit
+          scelerisque ultricies. Nulla facilisi.
+        </p>
+      </Grid>
+    </Container>
   );
 }
 
