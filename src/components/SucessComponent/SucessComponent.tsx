067bfe60a97feda01d8b10119bd64b69e2a1c80e
paulohenrique-amaral <paulopp.amaral@gmail.com>
2024-07-31 19:44:31 -0300

    style: update style

--- src/components/SucessComponent/SucessComponent.tsx
+++ src/components/SucessComponent/SucessComponent.tsx
@@ -1,25 +1,37 @@
-// import { useContext } from 'react';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Box, Typography, Button } from '@mui/material';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
-// import Context from '../../context/Context';
+import Context from '../../context/Context';
 import { BoxStyled } from './SucessComponentStyled';
+import { initialState } from '../../help/helper';
 
 type SucessComponentProps = {
   handleClose: () => void,
 };
 
 function SucessComponent({ handleClose }: SucessComponentProps) {
-  // const { personData, setPersonData, setEtapaAtual } = useContext(Context);
+  const { personData, setPersonData, clearCart } = useContext(Context);
+  const navigate = useNavigate();
   return (
     <BoxStyled>
-      <Typography variant="body1">
-        Parabéns seu pedido foi concluído com sucesso!
+      <Typography variant="body1" textAlign="center">
+        {`Parabéns ${personData.person.name}, seu pedido foi realizado com sucesso!`}
+      </Typography>
+      <Typography variant="body2" textAlign="center">
+        {`Os detalhes da sua compra foram enviados 
+        para o email ${personData.person.email}`}
       </Typography>
       <Box sx={ { margin: '50px' } }>
         <ThumbUpIcon sx={ { fontSize: 150 } } />
       </Box>
       <Button
-        onClick={ () => handleClose() }
+        onClick={ () => {
+          setPersonData(initialState);
+          clearCart();
+          navigate('/');
+          handleClose();
+        } }
         variant="outlined"
         color="primary"
       >
